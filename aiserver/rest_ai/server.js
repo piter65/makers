c54f69694888ec9c908eb7534fdccf263a88381c
Brent Chance <brent.m.chance@gmail.com>
2019-01-16 14:59:37 -0800

    - Rename alias for 'func.js' to 'f' in 'server.js'.

--- aiserver/rest_ai/server.js
+++ aiserver/rest_ai/server.js
@@ -24,21 +24,21 @@ const act_970 = require('./act_970');
 const act_980 = require('./act_980');
 const act_990 = require('./act_990');
 
-const func = require('./func');
+const f = require('./func');
 
 let testArr = ['alice', 'bob', 'cisco', 'dude'];
 
 
-// BChance: Peter these are tests for 'func' methods.
+// BChance: Peter these are tests for 'f' methods.
 //   Remove if you want.
-logger.log('includesAny | %s : %s', ['alice'].join(' '), func.includesAny(testArr, 'alice'));
-logger.log('includesAny | %s : %s', ['edward'].join(' '), func.includesAny(testArr, 'edward'));
-logger.log('includesAny | %s : %s', ['edward', 'greg', 'bob'].join(' '), func.includesAny(testArr, 'edward', 'greg', 'bob'));
+logger.log('includesAny | %s : %s', ['alice'].join(' '), f.includesAny(testArr, 'alice'));
+logger.log('includesAny | %s : %s', ['edward'].join(' '), f.includesAny(testArr, 'edward'));
+logger.log('includesAny | %s : %s', ['edward', 'greg', 'bob'].join(' '), f.includesAny(testArr, 'edward', 'greg', 'bob'));
 
-logger.log('includesAll | %s : %s', ['alice'].join(' '), func.includesAll(testArr, 'alice'));
-logger.log('includesAll | %s : %s', ['edward'].join(' '), func.includesAll(testArr, 'edward'));
-logger.log('includesAll | %s : %s', ['edward', 'greg', 'bob'].join(' '), func.includesAll(testArr, 'edward', 'greg', 'bob'));
-logger.log('includesAll | %s : %s', ['cisco', 'dude', 'alice'].join(' '), func.includesAll(testArr, 'cisco', 'dude', 'alice'));
+logger.log('includesAll | %s : %s', ['alice'].join(' '), f.includesAll(testArr, 'alice'));
+logger.log('includesAll | %s : %s', ['edward'].join(' '), f.includesAll(testArr, 'edward'));
+logger.log('includesAll | %s : %s', ['edward', 'greg', 'bob'].join(' '), f.includesAll(testArr, 'edward', 'greg', 'bob'));
+logger.log('includesAll | %s : %s', ['cisco', 'dude', 'alice'].join(' '), f.includesAll(testArr, 'cisco', 'dude', 'alice'));
 
 
 
