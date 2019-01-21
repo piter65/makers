bba1de734399174317946bd66fb3b409ea2f1ff0
Jeff Siegel <jeffsiegel@brandgames.com>
2019-01-20 19:47:04 -0500

    Good and Bad Path Added
    Added a good path and bad path for demo

--- aiserver/ai_tester/tests.js
+++ aiserver/ai_tester/tests.js
@@ -1,5 +1,28 @@
 var tests_origin =
 [
+	{ input:"JEFF'S PERFECT PATH FOR DEMO",reply:null},
+	{ input:"newgame",reply:"rp_5_intro"	},
+	{ input:"thanks, how can I help you",reply:"rp_10_offeredhelp"},
+	{ input:"what type of toppings do you like",reply:"rp_20_one_of_each"},
+	{ input:"ok what meat would you like?",reply:"rp_20_i_prefer_sausage"},
+	{ input:"great and what kind of veggie",reply:"rp_22_veg_hintb"},
+	{ input:"that sounds great, so one slice sausage and mushroom coming up",reply:"rp_3_remember_gluten"},
+	{ input:"why do you think that happened",reply:"rp_3_gluten_uncle"},
+	{ input:"no problem, we have a gluten free crust",reply:"rp_3_decided_nogluten"},
+	{ input:"great, so one slice gluten free with sausage and mushroom coming up",reply:"rp_go_finished_good2"},
+
+	{ input:"JEFF'S BAD SALESMANSHIP PATH FOR DEMO",reply:null},
+	{ input:"newgame",reply:"rp_5_intro"	},
+	{ input:"what do you want",reply:"rp_10_offeredhelp"},
+	{ input:"how about onions",reply:"rp_20_veg1"},
+	{ input:"how about tomato",reply:"rp_20_decide_mushroom"},
+	{ input:"how about meat ball",reply:"rp_24_meat_hint"},
+	{ input:"how about pepperoni",reply:"rp_24_meat_hint"},
+	{ input:"how about salami",reply:"rp_24_meat_giveup"},
+	{ input:"okay, one slice sausage and mushroom coming up",reply:"rp_3_remember_gluten"},
+	{ input:"sorry",reply:"rp_3_gluten_uncle"},
+  { input:"yeah, you should probably avoid pizza",reply:"rp_32_fed_up"},
+
 
 	{ input:"bug 43  Jan 19 - response hints at order",reply:null},
 	{ input:"newgame",reply:"rp_5_intro"	},
@@ -396,7 +419,7 @@ var tests_origin =
 
 
 
-{ input:"TOKENS, TOKENS, TOKENS, TOKENS FOR EVERYONE",reply:null},	
+{ input:"TOKENS, TOKENS, TOKENS, TOKENS FOR EVERYONE",reply:null},
 { input:"newgame robot",reply:"rp_5_intro"},
 
 { input:"#I feel sorry for you",reply:"tokens"},
