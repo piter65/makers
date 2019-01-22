9ae64017331e976623071276f5f7e6f2231043ca
Peter Atoms <piter65@protonmail.com>
2019-01-22 11:15:56 -0800

    Update config.js

--- aiserver/ai_tester/config.js
+++ aiserver/ai_tester/config.js
@@ -3,8 +3,8 @@ const test_sets = {};
 
 const config =
 {
-	server: 'http://localhost',
-//   server: 'http://54.202.22.196',
+//	server: 'http://localhost',
+   server: 'http://54.202.22.196',
 
 	test_set: 'tests_peter',
 	// test_set: 'tests_jeff',
